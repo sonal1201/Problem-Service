29bd8856fdbeca9e0d6bf0300de4cd07daf6af7c
Sonal Singh <sonal1201kumar@gmail.com>
2024-11-16 15:57:55 +0530

    Feat: add not implemented error for api

--- src/controllers/problemController.js
+++ src/controllers/problemController.js
@@ -1,24 +1,41 @@
-function ping(req,res){
-    return res.send({msg:"Ping Checked"})
+function ping(req, res) {
+    return res.send({ msg: "Ping Checked" })
 }
 
 function addProblem(req, res) {
 
+    return res.status(501).json({
+        Message: "Not Implemented"
+    })
+
 }
 
 function getProblem(req, res) {
+    return res.status(501).json({
+        Message: "Not Implemented"
+    })
 
 }
 
 function getProblems(req, res) {
+    return res.status(501).json({
+        Message: "Not Implemented"
+    })
 
 }
 
 function deleteProblem(req, res) {
+    return res.status(501).json({
+        Message: "Not Implemented"
+    })
+
 
 }
 
 function updateProblem(req, res) {
+    return res.status(501).json({
+        Message: "Not Implemented"
+    })
 
 }
 
